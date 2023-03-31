b730c4d8fb2f5b30df6e8cd421f4e2c0d700a3d5
Nelson <david.saber44@gmail.com>
2023-03-30 23:59:47 -0500

    Eliminacion de dependencias innecesarias

--- BilleteraApp/ReadPocket.cs
+++ BilleteraApp/ReadPocket.cs
@@ -1,16 +1,11 @@
 ﻿using Android.App;
 using Android.Content;
 using Android.OS;
-using Android.Runtime;
 using Android.Views;
 using Android.Widget;
-using Java.Lang;
-using Java.Util.Logging;
-using Java.Util.Zip;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace BilleteraApp
 {
